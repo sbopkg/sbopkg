bb9feddf39286268bb0000e0120405990b6237f1
slakmagik <slakmagik@18908463-3d48-0410-af50-6b38eb639d31>
2011-02-05 03:18:48 +0000

    revise documentation
    Major reformatting and some textual modifications of
    src/usr/doc/sbopkg-*/* including breaking out the credits from the
    script into a detailed THANKS file, renaming and reformatting
    ChangeLog.txt into NEWS, renaming the sample queuefiles from *.sqf to
    *.sqf.sample, and making lesser modifications to all the other docs.

--- trunk/src/usr/doc/README-renames.d
+++ trunk/src/usr/doc/README-renames.d
@@ -1,14 +1,20 @@
-# $Id$
-# vim:set ft=:
+README-renames.d
 
 The /etc/sbopkg/renames.d directory is the directory that holds files
-associated with renamed software in the SlackBuilds.org repository.  The
-default file that is installed with sbopkg is '50-default'.  This file will be
-overwritten each time sbopkg is upgraded, so do not make local edits to it.
-Instead, create other files with a higher or lower number than 50-default and
-list your renamed files in those local files.  The files are loaded in
-alphabetical order (like the files in /etc/fonts/conf.d) so 'priority'
-is determined from low number to high and the first match wins, so:
+associated with renamed software in the SlackBuilds.org repository.
+
+Renames are written, one per line, in the following format:
+
+oldname=newname
+
+The default file that is installed with sbopkg is '50-default'. This
+file will be overwritten each time sbopkg is upgraded, so do not make
+local edits to it. Instead, create other files with a higher or lower
+number than 50-default and list your renamed files in those local files.
+
+The files are loaded in alphabetical order (like the files in, e.g.,
+/etc/fonts/conf.d) so 'priority' is determined from low number to high
+and the first match wins, so:
 
 $ cat /etc/sbopkg/renames.d/10-local
 foo=bar
@@ -18,7 +24,3 @@ will override anything in a higher-numbered file, including anything in
 
 $ cat /etc/sbopkg/renames.d/50-default
 foo=baz
-
-Renames are written in the following format:
-
-oldname=newname
--- trunk/src/usr/doc/README-repos.d
+++ trunk/src/usr/doc/README-repos.d
@@ -1,40 +1,49 @@
-# $Id$
-# vim:set ft=:
+README-repos.d
 
-In sbopkg parlance, a "repository" is a local or remote service used as a
-source of scripts.  For example, slackbuilds.org is a repository.  The
-builds.slamd64.com project is another repository.  Every repository has one or
-more "branches".  Branches consist of a single tree of scripts.  For example,
-slackbuilds.org has a 11.0 branch, a 12.0 branch and so on.
+In sbopkg parlance, a "repository" is a local or remote service used as
+a source of SlackBuilds. For example, slackbuilds.org is a repository.
+The builds.slamd64.com project is another repository. Every repository
+has one or more "branches". Branches consist of a single tree of scripts
+in a category/application hierarchy. For example, slackbuilds.org has a
+11.0 branch, a 12.0 branch and so on.
 
 /etc/sbopkg/repos.d is a directory containing files defining the
-sbopkg-supported repositories and branches.  All *.repo files are scanned in
-alphabetical order.  Every line in a *.repo file defines a branch.  Each line
-is compound of the following seven fields:
-
-1.  REPOSITORY (a _short_ name identifying the repository)
-2.  BRANCH (a _short_ name identifying the branch of that repository)
-3.  DESCRIPTION (a <50 chars description, which _must_be_quoted_)
-4.  TAG (the packages' tag)
-5.  TOOL (rsync, git or "", is the tool able to check out the repository/branch)
-6.  LINK (the tool-dependent link to the branch)
-7.  CHECKGPG (whether the repo provides .asc signature files and signed
-    tarballs that can be verified with GPG)
-
-For example, one branch (line) of the sbo.repo file might look like this (note
-the seven fields):
-
-SBo 12.2 "SBo repository for Slackware 12.2" _SBo rsync slackbuilds.org::slackbuilds/12.2 GPG
-
-If TOOL is set to "", then it will not be possible to automatically update the
-branch (i.e., it has no upstream).  This is most commonly used for
-locally-maintained repositories on the host filesystem that do not use rsync
-or git to pull down the repository tree.  The LINK format is essentially what
-is required to be passed to the specified TOOL.  In case of git links, it
-_must_ be in the form url@branch.  If TOOL is "", LINK is ignored (but _must_
-still be present).  CHECKGPG format can be "GPG" if the repo supports GPG
-checking, or "" (which also must be present) if the repo does not support GPG
-checks.
-
-Lines _containing_ # are ignored when parsing the files.  Lines containing
+repositories and branches sbopkg will use. All *.repo files are scanned
+in alphabetical order. Every line in a *.repo file defines a branch
+except those _containing_ a '#' which are ignored. Lines containing
 backslashes (\) are not allowed.
+
+Each line contains the following seven fields (if a field is not
+applicable, it still _must_ be present in the form of an empty quote
+(""):
+
+1. REPO
+    A _short_ name identifying the repository; e.g., 'SBo'.
+2. BRANCH
+    A _short_ name identifying the branch of that repository; e.g.,
+    '11.0'.
+3. DESCRIPTION
+    A description of the repo, which _must be quoted_ and must be less
+    than 50 characters long; e.g., '"SBo repository for Slackware
+    11.0"'.
+4. TAG
+    The tag the built packages will have; e.g., '_SBo'.
+5. TOOL
+    The tools used to download the repository/branch. The currently
+    supported value is 'rsync'. Using 'git' is also possible. Local
+    repos will have an empty quote. If this field is an empty quote, it
+    will not be possible to automatically update the branch (i.e., it
+    has no upstream).
+6. LINK
+    The link (URL) to the branch that the TOOL will use. Local repos
+    will have an empty quote. In the case of git links, it _must_ be in
+    the form url@branch.
+7. CHECKGPG
+    Filled with 'GPG' if the repo provides .asc signature files and
+    signed tarballs that can be verified with GPG, and an empty quote
+    otherwise.
+
+For example, one branch (line) of the sbo.repo file looks like this
+(note the seven fields):
+
+SBo 11.0 "SBo repository for Slackware 11.0" _SBo rsync slackbuilds.org::slackbuilds/11.0 GPG
